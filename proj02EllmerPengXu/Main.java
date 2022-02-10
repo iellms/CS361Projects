946b716f7865f036fc25a5b72ff43c40a55637ae
Ian Ellmer <iaellm22@colby.edu>
2022-02-10 13:19:20 -0500

    Setting Up for Proj02

---
+++ proj02EllmerPengXu/Main.java
@@ -0,0 +1,20 @@
+package proj02EllmerPengXu;
+
+import javafx.application.Application;
+//import javafx.application.Platform;
+//import javafx.scene.Scene;
+//import javafx.scene.control.*;
+//import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+//import javafx.scene.layout.VBox;
+
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage){
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
