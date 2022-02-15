19554de91bd1cbcfcef6a3e286c2cb7fa2d60cdb
Ian Ellmer <iaellm22@colby.edu>
2022-02-15 10:03:34 -0500

    Making project3 stuff ready

---
+++ proj03EllmerLoverudeQian/Main.java
@@ -0,0 +1,24 @@
+package proj03EllmerLoverudeQian;
+
+// import java.util.Optional;
+import javafx.application.Application;
+// import javafx.application.Platform;
+// import javafx.fxml.FXML;
+// import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+// // import javafx.scene.Parent;
+// import javafx.scene.Scene;
+// import javafx.scene.control.Button;
+// import javafx.scene.control.TextInputDialog;
+// import javafx.scene.control.TextArea;
+// import javafx.event.ActionEvent;
+
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage){
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
