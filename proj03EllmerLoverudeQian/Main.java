8ef841731dd27653edad05d17b43ca4cb52bca52
Ian Ellmer <iaellm22@colby.edu>
2022-02-16 13:27:03 -0500

    Adding Main.FXML and Main.css to run this

--- proj03EllmerLoverudeQian/Main.java
+++ proj03EllmerLoverudeQian/Main.java
@@ -4,10 +4,10 @@ package proj03EllmerLoverudeQian;
 import javafx.application.Application;
 // import javafx.application.Platform;
 // import javafx.fxml.FXML;
-// import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-// // import javafx.scene.Parent;
-// import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 // import javafx.scene.control.Button;
 // import javafx.scene.control.TextInputDialog;
 // import javafx.scene.control.TextArea;
@@ -15,7 +15,24 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
     @Override
-    public void start(Stage primaryStage){
+    public void start(Stage primaryStage) throws java.io.IOException {
+
+        // Load fxml file
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        primaryStage.setTitle("Project 2");
+        Scene scene = new Scene(root);
+
+        // Load css file
+        scene.getStylesheets().add(getClass().getResource("Main.css").toExternalForm());
+        primaryStage.setScene(scene);
+
+        // Set the minimum height and width of thmaine stage
+        primaryStage.setMinHeight(250);
+        primaryStage.setMinWidth(300);
+
+        // Show the stage
+        primaryStage.show();
+
     }
 
     public static void main(String[] args){
