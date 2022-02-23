d1c88880e4e2d72b4d6f7b8969160348e5321a32
KitchenTable99 <ccbitt23@colby.edu>
2022-02-23 17:08:32 -0500

    linted

--- proj04BittingEllmerWang/Main.java
+++ proj04BittingEllmerWang/Main.java
@@ -11,7 +11,6 @@ package proj04BittingEllmerWang;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
