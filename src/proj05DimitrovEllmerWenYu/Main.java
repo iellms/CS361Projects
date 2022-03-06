7530c51accf2fe19b36c256ed53af905f7960484
Ian Ellmer <iaellm22@colby.edu>
2022-03-06 18:52:04 -0500

    Handling IOException

--- src/proj05DimitrovEllmerWenYu/Main.java
+++ src/proj05DimitrovEllmerWenYu/Main.java
@@ -12,6 +12,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 import java.io.IOException;
 
@@ -29,15 +31,28 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        Controller controller = fxmlLoader.getController();
+    public void start(Stage primaryStage){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+            Controller controller = fxmlLoader.getController();
 
-        primaryStage.setTitle("Alex, Anton, Ian, and Muqing's Project 5");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        // exit method triggered when user tries to click on the red dot on top corner
-        primaryStage.setOnCloseRequest(controller::handleExitMenuItem);
+            primaryStage.setTitle("Alex, Anton, Ian, and Muqing's Project 5");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            // exit method triggered when user tries to click on the red dot on top corner
+            primaryStage.setOnCloseRequest(controller::handleExitMenuItem);
+        } catch(IOException e){
+            Alert ioAlert = new Alert(AlertType.ERROR);
+            ioAlert.setTitle("Error");
+            ioAlert.setHeaderText("Error Opening This Application");
+
+            ioAlert.setContentText(
+                "We encountered a IO problem opening this Application.  Please " +
+                "try again or restart your machine."
+                );
+
+            ioAlert.show();
+        }
     }
 }
